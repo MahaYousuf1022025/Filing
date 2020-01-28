8e38f7be8bf61da89344e3d781efbbfdc2fa21fb
MahaYousuf1022025 <55849128+MahaYousuf1022025@users.noreply.github.com>
2020-01-28 21:32:08 +0500

    Add files via upload

---
+++ search record.vb
@@ -0,0 +1,44 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim name As String
+        Dim MemberId As String
+        Dim value As String
+        Dim isfound As Boolean
+
+        isfound = False
+
+        name = ""
+        MemberId = ""
+        Console.WriteLine("enter member to be searched")
+        value = Console.ReadLine
+
+
+        FileOpen(1, "c:\filepractice\ABC.txt", OpenMode.Input)
+
+        While Not EOF(1)
+            Input(1, name)
+            Input(1, MemberId)
+            Console.WriteLine(name)
+            Console.WriteLine(MemberId)
+            If name = value Then
+                isfound = True
+                Console.WriteLine("member's id: " & MemberId)
+
+            End If
+
+        End While
+
+        FileClose(1)
+        If isfound = False Then
+            Console.WriteLine("sorry your member is not found")
+        End If
+        Console.ReadKey()
+
+
+
+
+
+    End Sub
+
+End Module
