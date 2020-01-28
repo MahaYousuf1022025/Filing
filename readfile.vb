a48ba75110ac7358f5d71e102118a950a5d772b3
MahaYousuf1022025 <55849128+MahaYousuf1022025@users.noreply.github.com>
2020-01-28 20:41:06 +0500

    Add files via upload

---
+++ readfile.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim name As String
+        Dim MemberId As String
+
+
+        name = ""
+        MemberId = ""
+
+        FileOpen(1, "c:\filepractice\ABC.txt", OpenMode.Input)
+
+        While Not EOF(1)
+            Input(1, name)
+            Input(1, MemberId)
+            Console.WriteLine(name)
+            Console.WriteLine(MemberId)
+
+        End While
+
+
+
+        FileClose(1)
+        Console.ReadKey()
+
+
+
+
+
+    End Sub
+
+End Module
