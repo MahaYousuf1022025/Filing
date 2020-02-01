d62d55e05b15e9515ffd0896b60eb75f3d57264c
MahaYousuf1022025 <55849128+MahaYousuf1022025@users.noreply.github.com>
2020-02-01 16:13:27 +0500

    Add files via upload

---
+++ additional records.vb
@@ -0,0 +1,41 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim name As String
+        Dim MemberId As String
+        Dim telephonenum As String
+        Dim membership As String
+
+
+        name = ""
+        MemberId = ""
+
+        FileOpen(1, "c:\filepractice\ABC.txt", OpenMode.Input)
+        FileOpen(2, "c:\filepractice\ABC2.txt", OpenMode.Output)
+
+        While Not EOF(1)
+            Input(1, name)
+            Input(1, MemberId)
+            Console.WriteLine(name)
+            Console.WriteLine(MemberId)
+            Console.WriteLine("enter your telephone number")
+            telephonenum = Console.ReadLine
+            Console.WriteLine("enter membership start date")
+            membership = Console.ReadLine
+
+            WriteLine(2, name)
+            WriteLine(2, MemberId)
+            WriteLine(2, telephonenum)
+            WriteLine(2, membership)
+
+
+
+        End While
+        FileClose(2)
+
+        FileClose(1)
+        Console.ReadKey()
+    End Sub
+
+End Module
