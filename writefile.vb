ed6214da2a9c32294547c42c174d03a9834ab12f
MahaYousuf1022025 <55849128+MahaYousuf1022025@users.noreply.github.com>
2020-02-01 19:31:26 +0500

    Add files via upload

---
+++ writefile.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim name As String
+        Dim MemberId As String
+        Dim counter As Integer
+
+
+
+        name = ""
+        MemberId = ""
+        Console.WriteLine("Enter the number of members for which data is to be recorded")
+        counter = Console.ReadLine
+
+
+        FileOpen(1, "c:\filepractice\ABC.txt", OpenMode.Output)
+        For count = 1 To counter
+            Console.WriteLine("enter your members names")
+            name = Console.ReadLine
+            Console.WriteLine("enter members id")
+            MemberId = Console.ReadLine
+
+            WriteLine(1, name)
+            WriteLine(1, MemberId)
+        Next
+
+        FileClose(1)
+        Console.ReadKey()
+    End Sub
+
+End Module
